7974891be1213de9c6dfbc4a9833f89d1b1c6ba5
liamkyoung <liamkingstonyoung@gmail.com>
2024-07-10 13:10:20 -0400

    needed to change dto in dummy data

--- data/playerData.ts
+++ data/playerData.ts
@@ -2,117 +2,13 @@ import { PlayerProfile } from '@/types/viewmodels/types'
 
 export const s1mple: PlayerProfile = {
   uri: '/players/s1mple',
-  team: {
-    id: 1,
-    name: 'NaVi',
-    hltvProfile: 'https://www.hltv.com/',
-    hltvRanking: 2,
-    coachName: 'Blade',
-    timeSinceLastUpdated: new Date(),
-  },
+  teamName: 'NaVi',
+  teamProfileImage:
+    'https://img-cdn.hltv.org/teamlogo/9iMirAi7ArBLNU8p3kqUTZ.svg?ixlib=java-2.1.0&s=4dd8635be16122656093ae9884675d0c',
   playerName: 's1mple',
   countryAbbreviation: 'UK',
   profileImage:
     'https://img-cdn.hltv.org/playerbodyshot/4NNsZrSGWLr9mZNt0Pe3KS.png?ixlib=java-2.1.0&w=400&s=4484cc99087121a6f9877d3742717444',
 }
-
-// export const AleksiB: PlayerProfile = {
-//   uri: '/players/aleksib',
-//   teamName: 'NaVi',
-//   playerName: 'AleksiB',
-//   flag: 'FN',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/CS5O_NY6wiy7_JG2V2GWYD.png?ixlib=java-2.1.0&w=400&s=b7bcd079508c8f59cc1bdf73cfd404bc',
-// }
-
-// export const iM: PlayerProfile = {
-//   uri: '/players/iM',
-//   teamName: 'NaVi',
-//   playerName: 'iM',
-//   flag: 'RM',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/KbJzukpxyrqGUtvR571r1Z.png?ixlib=java-2.1.0&w=400&s=851a2e8cd2886d5a8432be947ce28755',
-// }
-
-// export const b1t: PlayerProfile = {
-//   uri: '/players/b1t',
-//   teamName: 'NaVi',
-//   playerName: 'b1t',
-//   flag: 'UK',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/5ce_52SMMLOXUPHFmuppeo.png?ixlib=java-2.1.0&w=400&s=e33f0eef5a3b048c1125777bf8aa72c3',
-// }
-
-// export const jL: PlayerProfile = {
-//   uri: '/players/jL',
-//   teamName: 'NaVi',
-//   playerName: 'jL',
-//   flag: 'LH',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/LEVIO874MT6HpnUZqZgIAa.png?ixlib=java-2.1.0&w=400&s=32990b70b7fadcbc772538b5fb017f75',
-// }
-
-// export const w0nderful: PlayerProfile = {
-//   uri: '/players/w0nderful',
-//   team: 'NaVi',
-//   playerName: 'w0nderful',
-//   flag: 'UK',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/-_UTOwRq10TZ6Fzs_Gp_m1.png?ixlib=java-2.1.0&w=400&s=07067310960a920155ce725b27969258',
-// }
-
-// export const NiKo: PlayerProfile = {
-//   uri: '/players/niko',
-//   teamName: 'G2',
-//   playerName: 'NiKo',
-//   flag: 'BS',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/eefPmsiMIo4dAiiPUmZE6-.png?ixlib=java-2.1.0&w=400&s=8d9765f9d2c40c13d7bd6c96c45a2849',
-// }
-
-// export const hunter: PlayerProfile = {
-//   uri: '/players/huNter-',
-//   teamName: 'G2',
-//   playerName: 'huNter-',
-//   flag: 'BS',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/3dH0n73hE5olzCgXPetK_H.png?ixlib=java-2.1.0&w=400&s=77f0dc3e6ca0595afbf0db6c9f7ae0de',
-// }
-
-// export const HooXi: PlayerProfile = {
-//   uri: '/players/hooxi',
-//   teamName: 'G2',
-//   playerName: 'HooXi',
-//   flag: 'BS',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/oaDMbSKLWf0TMu0qIQ4_xj.png?ixlib=java-2.1.0&w=400&s=a19191dc16c63d503d88af65a652d2b0',
-// }
-
-// export const m0nesy: PlayerProfile = {
-//   uri: '/players/m0nesy',
-//   teamName: 'G2',
-//   playerName: 'm0nesy',
-//   flag: 'RU',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/F8-kMfRKbV5UDnMo4elY5J.png?ixlib=java-2.1.0&w=400&s=8b740a6d3e55871a8d773b298b486e8c',
-// }
-
-// export const dev1ce: PlayerProfile = {
-//   uri: '/players/dev1ce',
-//   teamName: 'Astralis',
-//   playerName: 'dev1ce',
-//   flag: 'BS',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/K9M49xoheDwWbY8E1pjkna.png?ixlib=java-2.1.0&w=400&s=185342cc45b043d003fa2e22a907b1d4',
-// }
-
-// export const ropz: PlayerProfile = {
-//   uri: '/players/ropz',
-//   teamName: 'FaZe',
-//   playerName: 'ropz',
-//   flag: 'ES',
-//   imgSrc:
-//     'https://img-cdn.hltv.org/playerbodyshot/z7mlBNbUIhdpFu61k9Z7Ao.png?ixlib=java-2.1.0&w=400&s=4079a3406072a2f6c0afbe74d33aa810',
-// }
 
 export const DummyPlayers: PlayerProfile[] = [s1mple]
